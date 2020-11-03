254686a011fde358d3b337cdf9cc3cde6f4e20a7
devansh5 <devkumar5436@gmail.com>
2020-11-04 00:24:46 +0530

    3d-cube using three.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
-
+import React from 'react';
+import {Canvas} from 'react-three-fiber';
+import Box from './components/Mesh';
 function App() {
+  const style={
+    width:"100%",
+    height:"800px",
+    backgroundColor:"black"
+  }
   return (
-    <div className="App">
-      <h1>Mine Sweeper</h1>
-    </div>
-  );
+    <Canvas style={style}>
+    <ambientLight/>
+    <pointLight position={[-1,2, 4]} />
+    <Box position={[0, 0, 0]} />
+    
+ </Canvas>
+  )
 }
-
 export default App;
